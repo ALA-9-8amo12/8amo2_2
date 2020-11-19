552214027f815fe592ee68a25cf21f0bf7ca71c9
CrazyVito11 <31567466+CrazyVito11@users.noreply.github.com>
2020-11-19 10:47:13 +0100

    Refactor

--- app/src/main/java/com/example/amazighapp/MainMenuActivity.java
+++ app/src/main/java/com/example/amazighapp/MainMenuActivity.java
@@ -1,15 +1,16 @@
 package com.example.amazighapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_menu);
     }
+
 }
