97e8e858d2953379716c629cb74d3195c4ad4113
HaasterenvanChristiaan8amo2 <1043218@mborijnland.nl>
2020-11-19 11:24:49 +0100

    Splashscreen done

--- app/src/main/java/com/example/amazighapp/MainMenuActivity.java
+++ app/src/main/java/com/example/amazighapp/MainMenuActivity.java
@@ -8,7 +8,7 @@ public class MainMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
