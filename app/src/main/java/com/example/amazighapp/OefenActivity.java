b5b2ca9f8da9bc0175a8e9dda347b7669e001da2
HaasterenvanChristiaan8amo2 <1043218@mborijnland.nl>
2020-12-15 15:11:29 +0100

    Was dit vergeten te pushen met de vorige commit

--- app/src/main/java/com/example/amazighapp/OefenActivity.java
+++ app/src/main/java/com/example/amazighapp/OefenActivity.java
@@ -2,13 +2,11 @@ package com.example.amazighapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 import android.util.Log;
 
-import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,16 +19,20 @@ public class OefenActivity extends AppCompatActivity {
 
     DatabaseReference mBase;
     Integer CategoryId;
+    TranslatedWordAdapter adapter;
     ArrayList WordList;
+    ViewPager2 viewpager;
+    String ama, ned, image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spelen);
+        setContentView(R.layout.activity_oefen);
 
         CategoryId = Integer.parseInt(getIntent().getStringExtra("CATEGORY_ID"));
+        viewpager = findViewById(R.id.viewPager2);
 
         mBase = (DatabaseReference) FirebaseDatabase.getInstance().getReference().child("translated_word");
         mBase.addListenerForSingleValueEvent(
@@ -46,15 +48,41 @@ public class OefenActivity extends AppCompatActivity {
                                 continue;
                             }
 
+                            ama = translatedWord.getWord_ama();
+                            ned = translatedWord.getWord_ned();
+                            image = translatedWord.getImage_url();
+
                             TranslatedWords.add(translatedWord);
+                            Log.d("WordEntry", TranslatedWordSnapshot.toString());
+                            Log.d("TransWords", String.valueOf(TranslatedWords));
+                            Log.d("WordEntryValue", translatedWord.toString());
                         }
 
                         WordList = TranslatedWords;
+                        for (Object WordObject: WordList) {
+
+                        }
+                        Log.d("WordEntryValues", ama + " " + ned + " " + image);
+                        Log.d("WordList", WordList.toString());
+                        adapter = new TranslatedWordAdapter(WordList);
                     }
 
                     @Override public void onCancelled(@NonNull DatabaseError error) {
                         Log.e("OefenActivity", "Firebase Error: " + error.getMessage());
                     }
         });
+        viewpager.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
     }
 }
--- app/src/main/java/com/example/amazighapp/TranslatedWordAdapter.java
+++ app/src/main/java/com/example/amazighapp/TranslatedWordAdapter.java
@@ -1,4 +1,68 @@
 package com.example.amazighapp;
 
-public class TranslatedWordAdapter {
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+
+import java.util.ArrayList;
+import android.widget.RelativeLayout;
+
+public class TranslatedWordAdapter extends RecyclerView.Adapter<TranslatedWordAdapter.TrWordViewHolder> {
+
+    ArrayList<Object> Words;
+
+    public TranslatedWordAdapter(ArrayList wordList) {
+        Words = wordList;
+        Log.d("Wordlist Content", String.valueOf(Words));
+    }
+
+    static class TrWordViewHolder extends RecyclerView.ViewHolder {
+        TextView wordNed, wordAmg;
+        ImageView wordImage;
+        Button btnSound;
+        RelativeLayout relativeLayout;
+
+        public TrWordViewHolder(@NonNull View itemView) {
+            super(itemView);
+            relativeLayout = itemView.findViewById(R.id.container);
+            wordNed = itemView.findViewById(R.id.wordNed);
+            wordAmg = itemView.findViewById(R.id.wordAmg);
+            btnSound = itemView.findViewById(R.id.btnSound);
+            wordImage = itemView.findViewById(R.id.wordImage);
+        }
+    }
+
+    public TrWordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.translated_word, parent, false);
+        return new TranslatedWordAdapter.TrWordViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TrWordViewHolder holder, int position) {
+        String woord = (String) Words.get(position);
+        holder.wordNed.setText("Nederlands");
+        holder.wordAmg.setText("Amazigh");
+//        holder.Date.setText("Date: " + model.getDate());
+
+        Glide.with(holder.itemView.getContext())
+                .load(R.drawable._general_logo)
+                .into(holder.wordImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return Words.size();
+    }
+
 }
